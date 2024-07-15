8fdb959401be0f40f6992dfff0082a8d5a9a11d1
VictoQ <victorbarbay1337@gmail.com>
2024-07-15 02:46:53 +0200

    exercise 4

--- mapper.py
+++ mapper.py
@@ -1,8 +1,11 @@
+
 import sys
+
+valid_categories = {"Computers", "Cameras", "Video Games"}
 
 for line in sys.stdin:
     data = line.strip().split("\t")
-    if len(data) != 6:
-        raise ValueError("Invalid input line: " + line)
-    date, time, item, category, sales, payment = data
-    sys.stdout.write("{0}\t{1}\n".format(category, sales))
+    if len(data) == 6:
+        date, time, item, category, sales, payment = data
+        if category in valid_categories:
+            sys.stdout.write("{0}\t{1}\n".format(category, sales))
