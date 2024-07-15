ab2b510142c764dcda19a00ab5ce82166d90b3f7
VictoQ <victorbarbay1337@gmail.com>
2024-07-15 02:46:20 +0200

    exercise 3

--- mapper.py
+++ mapper.py
@@ -1,8 +1,8 @@
-
 import sys
 
 for line in sys.stdin:
     data = line.strip().split("\t")
-    if len(data) == 6:
-        date, time, item, category, sales, payment = data
-        sys.stdout.write("{0}\t{1}\n".format(category, sales))
+    if len(data) != 6:
+        raise ValueError("Invalid input line: " + line)
+    date, time, item, category, sales, payment = data
+    sys.stdout.write("{0}\t{1}\n".format(category, sales))
