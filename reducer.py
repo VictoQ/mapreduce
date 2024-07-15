9df3470918170d3cbbefde587d3341f915492b35
VictoQ <victorbarbay1337@gmail.com>
2024-07-15 02:48:26 +0200

    exercise 6

--- reducer.py
+++ reducer.py
@@ -1,19 +1,25 @@
 import sys
 
-sum_of_values = 0
-previous_key = None
+current_category = None
+current_sum = 0
+current_count = 0
 
 for line in sys.stdin:
     data = line.strip().split("\t")
     key, value = data
+    value = float(value)
 
-    if previous_key != None and previous_key != key:
-        if sum_of_values > 114:
-            sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
-        sum_of_values = 0
+    if current_category == key:
+        current_sum += value
+        current_count += 1
+    else:
+        if current_category:
+            average = current_sum / current_count
+            sys.stdout.write("{0}\t{1}\n".format(current_category, average))
+        current_category = key
+        current_sum = value
+        current_count = 1
 
-    sum_of_values += 1
-    previous_key = key
-
-if sum_of_values > 114:
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, sum_of_values))
+if current_category:
+    average = current_sum / current_count
+    sys.stdout.write("{0}\t{1}\n".format(current_category, average))
